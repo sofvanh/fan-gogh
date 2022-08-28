a635d615930504bde6eeb60ee4cb27d221e58ac1
sofvanh <23138848+sofvanh@users.noreply.github.com>
2022-08-28 18:22:07 +0300

    Update theme, cleanup code

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# Fan Gogh
-*The project of someone who is a fan of Van Gogh, using AI to make paintings, some of them portraying fans, in the style of Van Gogh (and other impressionist and post-impressionist masters)*
+# Fan Gogh 🎨
+*As a fan of impressionist and post-impressionist art, I want to see more of it come to existence.
+So, I'm telling an AI to make some.*
+
+Live [here!](https://sofvanh.github.io/fan-gogh/)
 
 - All paintings created by me (@sofvanh) with [Midjourney](https://www.midjourney.com) unless otherwise noted
 - Art free to use, credit appreciated
-- Feel free to contact me wherever for art-related reasons
 
-<img src="src/assets/fan.png" alt="AI-generated 'oil painting' of a 'fan'" width="400"/>
+## About
+
+A small side project to learn web dev, design, and art history, and to play around with making AI art.
+
+Credit:
+- [Code With Yd](https://www.youtube.com/watch?v=cDwa_JwuC-w) for responsive image gallery tutorial
+- [Midjourney](https://www.midjourney.com) for the AI art model
--- src/App.js
+++ src/App.js
@@ -1,89 +1,14 @@
-import React, {useState} from 'react'
 import './App.css';
-import fan from './assets/fan.png'
-import skyline from './assets/skyline.png'
-import eiffel from './assets/eiffel_tower_claude_monet.png'
-import flower from './assets/flower_garden_paul_cezanne.png'
-import house from './assets/house_claude_monet.png'
-import shop from './assets/shop_with_lamps_claude_monet.png'
-import square from './assets/town_square_paul_cezanne.png'
-import view from './assets/view_from_mountain_paul_cezanne.png'
-
-import {ReactComponent as CloseIcon} from './assets/icons/close.svg'
 
 function App() {
     return (
         <div className="App">
-            <h1>Fan Gogh - AI-generated impressionist art</h1>
+            <h1 className="App-header">Fan Gogh - AI-generated impressionist art</h1>
             <p>! Work in progress !</p>
             <p>All paintings done in Midjourney by @sofvanh unless otherwise noted</p>
             <p>Art free to use, credit appreciated</p>
-            <div className="flex-container">
-                <img src={fan} />
-                <img src={skyline} />
-                <img src={eiffel} />
-                <img src={flower} />
-                <img src={house} />
-                <img src={shop} />
-                <img src={square} />
-                <img src={view} />
-            </div>
         </div>
     );
 }
 
-const Gallery = () => {
-    let data = [
-        {
-            id: 1,
-            imgSrc: fan,
-        },
-        {
-            id: 2,
-            imgSrc: skyline,
-        },
-        {
-            id: 3,
-            imgSrc: view,
-        },
-        {
-            id: 4,
-            imgSrc: flower,
-        },
-        {
-            id: 5,
-            imgSrc: house,
-        },
-        {
-            id: 6,
-            imgSrc: shop,
-        }
-    ]
-    const [model, setModel] = useState(false);
-    const [tempImgSrc, setTempImgSrc] = useState('');
-
-    const getImg = (imgSrc) => {
-        setTempImgSrc(imgSrc);
-        setModel(true);
-    }
-
-    return (
-        <>
-        <div className={model? "model open": "model"}>
-            <img src={tempImgSrc} />
-            <CloseIcon onClick={() => setModel(false)}/>
-        </div>
-            <div className="gallery">
-                {data.map((item, index) => {
-                    return (
-                        <div className="pic" key={index} onClick={() => getImg(item.imgSrc)}>
-                            <img src={item.imgSrc} style={{ width: '100%' }} />
-                        </div>
-                    )
-                })}
-            </div>
-        </>
-    )
-}
-
-export default Gallery;
+export default App;

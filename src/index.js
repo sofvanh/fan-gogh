5404180f5833bb2347f10376e205905de761da83
sofvanh <23138848+sofvanh@users.noreply.github.com>
2022-08-28 20:04:59 +0300

    Add .js
    IDK why this breaks anything

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Gallery from './Gallery';
+import App from './App.js';
+import Gallery from './Gallery.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
